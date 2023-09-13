7d03f02c032a3923eee174e20a501dd5611a4334
Mariusz <Mariusz@LAPTOP-92MQ158U>
2023-09-13 19:58:27 +0200

    Dodaj pliki projektów.

---
+++ AdvancedProgramming_Lesson4/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdvancedProgramming_Lesson4.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ChatMessage> ChatMessages { get; set; }
+    }
+}
